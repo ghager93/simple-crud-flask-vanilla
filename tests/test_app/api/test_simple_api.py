0e19f30f6f38e0da7edb92d80c9694316bbc24f3
ghager93 <ghager93@gmail.com>
2022-12-20 12:20:43 +0100

    Hello world test passing

--- tests/test_app/api/test_simple_api.py
+++ tests/test_app/api/test_simple_api.py
@@ -5,8 +5,8 @@ import flask
 from app import db
 from app.api import simple_api
 
-# def test_hello_world(app: flask.Flask):
-#     """Test handler works."""
-#     with app.test_request_context(method="GET"):
-#         result = simple_api.hello_world()
-#         assert result == "hello world!"
+def test_hello_world(app: flask.Flask):
+    """Test handler works."""
+    with app.test_request_context(method="GET"):
+        result = simple_api.hello_world()
+        assert result == "hello world!"
