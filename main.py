9bc3c1556dfc701802bab82db1c681d4c17c2770
ghager93 <ghager93@gmail.com>
2023-01-24 22:41:25 +1100

    Database initiated.

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine, Column, Integer
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 app = FastAPI()
 
+engine = create_engine("sqlite:///app.db")
+
+SessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+class Test(Base):
+    __tablename__ = "test"
+
+    test = Column(Integer, primary_key=True)
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 async def root():
